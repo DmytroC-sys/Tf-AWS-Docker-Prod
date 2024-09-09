f7b90eeb54e98c47bbf8ff19dab8a9a3f9722109
DmytroC-sys <mukomol007@gmail.com>
2024-09-09 23:48:53 +0200

    added networks a/b

--- Infra-TF/deploy/network.tf
+++ Infra-TF/deploy/network.tf
@@ -18,3 +18,73 @@ resource "aws_internet_gateway" "mainGatew" {
   }
 
 }
+########################################################
+#### public subnets for load balancer public access ####
+########################################################
+
+resource "aws_subnet" "public_a" {
+  vpc_id = aws_vpc.mainVPC
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags = {
+    name = "${local.prefix}-public-a"
+  }
+}
+
+resource "aws_route_table" "public_a" {
+  vpc_id = aws_vpc.mainVPC.id
+
+  tags = {
+    Name = "${local.prefix}-public-a"
+  }
+  
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_a.id
+  
+}
+
+resource "aws_route" "public_internet_access_a" {
+  route_table_id = aws_route_table.public_a.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.mainGatew.id
+}
+
+#########################################################
+#### Copy paste privious section for public access b ####
+#########################################################
+resource "aws_subnet" "public_b" {
+  vpc_id = aws_vpc.mainVPC
+  cidr_block = "10.1.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "${data.aws_region.current.name}b"
+
+  tags = {
+    name = "${local.prefix}-public-b"
+  }
+}
+
+resource "aws_route_table" "public_b" {
+  vpc_id = aws_vpc.mainVPC.id
+
+  tags = {
+    Name = "${local.prefix}-public-b"
+  }
+  
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public_b.id
+  
+}
+
+resource "aws_route" "public_internet_access_b" {
+  route_table_id = aws_route_table.public_b.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.mainGatew.id
+}
